2e9aa484c08696363e8a16e4c615a8a2819c6d66
anammaulana <133523637+anammaulana@users.noreply.github.com>
2025-02-18 15:31:14 +0700

    update 100 kali nya

--- src/app/page.js
+++ src/app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            hahah bro gw bisa juga yaa1
+            kontolodon
           </a>
         </div>
       </main>
