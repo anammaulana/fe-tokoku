6139dff7cddd5e8baf7a576bb9258744d7236b77
anammaulana <133523637+anammaulana@users.noreply.github.com>
2025-02-25 16:42:51 +0700

    Update page.js

--- src/app/products/edit/page.js
+++ src/app/products/edit/page.js
@@ -1,8 +1,11 @@
+import { Suspense } from "react";
 import EditProduct from "../../components/product/EditProduct";
 const EditProductPage = () =>{
     return (
         <>
+        <Suspense>
         <EditProduct/>
+        </Suspense>
         </>
     );
 }
